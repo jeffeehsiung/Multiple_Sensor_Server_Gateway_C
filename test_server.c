9c24ec355bd8ccc281a8deabb8134c9f5e95c97f
jeroen.vanaken <jeroen.vanaken@kuleuven.be>
2022-10-09 16:18:16 +0200

    added files for plab3connmgr

---
+++ test_server.c
@@ -0,0 +1,59 @@
+/**
+ * \author Jeffee Hsiung
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "config.h"
+#include "lib/tcpsock.h"
+
+#define PORT 5678
+#define MAX_CONN 3  // state the max. number of connections the server will handle before exiting
+
+
+/**
+ * Implements a sequential test server (only one connection at the same time)
+ */
+
+int main(void) {
+    tcpsock_t *server, *client;
+    sensor_data_t data;
+    int bytes, result;
+    int conn_counter = 0;
+
+    printf("Test server is started\n");
+    if (tcp_passive_open(&server, PORT) != TCP_NO_ERROR) exit(EXIT_FAILURE);
+    do {
+        if (tcp_wait_for_connection(server, &client) != TCP_NO_ERROR) exit(EXIT_FAILURE);
+        printf("Incoming client connection\n");
+        conn_counter++;
+        do {
+            // read sensor ID
+            bytes = sizeof(data.id);
+            result = tcp_receive(client, (void *) &data.id, &bytes);
+            // read temperature
+            bytes = sizeof(data.value);
+            result = tcp_receive(client, (void *) &data.value, &bytes);
+            // read timestamp
+            bytes = sizeof(data.ts);
+            result = tcp_receive(client, (void *) &data.ts, &bytes);
+            if ((result == TCP_NO_ERROR) && bytes) {
+                printf("sensor id = %" PRIu16 " - temperature = %g - timestamp = %ld\n", data.id, data.value,
+                       (long int) data.ts);
+            }
+        } while (result == TCP_NO_ERROR);
+        if (result == TCP_CONNECTION_CLOSED)
+            printf("Peer has closed connection\n");
+        else
+            printf("Error occured on connection to peer\n");
+        tcp_close(&client);
+    } while (conn_counter < MAX_CONN);
+    if (tcp_close(&server) != TCP_NO_ERROR) exit(EXIT_FAILURE);
+    printf("Test server is shutting down\n");
+    return 0;
+}
+
+
+
+
