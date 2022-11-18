5757ea4835d57d40a62d1d5a9b4f9caf15e71123
Jeffee Hsiung <chiehfei.hsiung@student.kuleuven.be>
2022-11-18 13:53:09 +0000

    sensor_db.c

---
+++ ex1/sensor_db.c
@@ -0,0 +1,5 @@
+/*Create a test program (main.c) and sensor_db module (the storage manager) to store temperature data into a text file, structured as a comma separated value file (csv). Implement both the header (sensor_db.h) and the implementation (sensor_db.c) with the following operations:
+• an operation to open a text file with a given name, and providing an indication if the file should be overwritten if the file already exists or if the data should be appended to the existing file.
+• an operation to append a single sensor reading to the csv file
+• an operation to close the csv file.
+*/
